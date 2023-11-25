7cd4f0d1c1dea346de6fb17926163c247c849c40
Gabriel <gabriel.rvs0@hotmail.com>
2023-11-25 20:38:30 -0300

    first commit

---
+++ user/views.py
@@ -0,0 +1,46 @@
+from django.shortcuts import render, HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from django.contrib.auth import login as auth_login
+from django.contrib.auth.decorators import login_required
+
+
+def register(request):
+    if request.method == "GET":
+        return render(request, 'register.html')
+    else:
+        username = request.POST.get('username')
+        firstname = request.POST.get('firstname')
+        lastname = request.POST.get('lastname')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        user = User.objects.filter(username=username).filter()
+        if user:
+            return HttpResponse('Ja existe um User com esse nome')
+
+        user = User.objects.create_user(username=username, first_name=firstname, last_name=lastname,
+                                        email=email, password=password)
+        user.save()
+
+        return HttpResponse(f'Usuario {username} Cadastrado com susseso ')
+
+
+def login(request):
+    if request.method == 'GET':
+        return render(request, 'login.html')
+    else:
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username, password=password)
+        if user:
+            auth_login(request, user)
+            return HttpResponse('Pode entrar zé')
+        else:
+            return HttpResponse('Não ache man ' + str(user))
+
+
+@login_required(login_url='auth/login/')
+def platform(request):
+    return render(request, 'login.html')
